e6404dd540e81bb8e8b3094b259f58ed6e1cfb4b
Zz-shouchen <z3437zz@outlook.com>
2025-01-20 14:11:18 +0800

    Rename abuild.sh to v1.0/abuild.sh

--- abuild.sh
+++
@@ -1,4 +0,0 @@
-make clean
-make
-./mygame
-# The above code will clean the project, build it, and run the game.
---
+++ v1.0/abuild.sh
@@ -0,0 +1,4 @@
+make clean
+make
+./mygame
+# The above code will clean the project, build it, and run the game.
