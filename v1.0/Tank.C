04f9798cf2d68c794ffbbe183459fda97d160d74
Zz-shouchen <z3437zz@outlook.com>
2025-01-20 14:32:33 +0800

    Update Tank.C

--- v1.0/Tank.C
+++ v1.0/Tank.C
@@ -10,14 +10,18 @@ bool Tank::out(){
 
 bool Tank::collide(int x, int y){
     if(toward==UP || toward==DOWN){
-        for(int i=-1;i<2;i++){if(x==row && y==col+i){
-            return true;
-        }}
+        for(int i=-1;i<2;i++){
+            if(x==row && y==col+i){
+                return true;
+            }
+        }
     }
     else if(toward==LEFT || toward==RIGHT){
-        for(int i=-1;i<2;i++){if(x==row+i && y==col){
-            return true;
-        }}
+        for(int i=-1;i<2;i++){
+            if(x==row+i && y==col){
+                return true;
+            }
+        }
     }
     return false;
 }
